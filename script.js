75f3d0d125fe7205d94e743329129441927e1827
Phantom <fay.ashbro@gmail.com>
2020-09-02 21:49:07 -0400

    pseudocode and beggining of generatepassword function

--- script.js
+++ script.js
@@ -12,3 +12,40 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+function generatePassword() {
+  
+  // length between 8-128
+  passwordLength = prompt("Enter a length between 8 and 128 characters:")
+  if(passwordLength <8 || passwordLength > 128){
+    prompt("Enter")
+  } // else move on 
+
+  // character types 
+// lowercase
+lowerPassword = prompt("")
+//uppercase
+// // upperPassword = prompt 
+// numeric
+// // num = prompt 
+// special characters 
+// // specChar = 
+
+
+}
+
+
+//   // prompts for password 
+
+// 
+
+
+// conditional statement for all prompts to be anwnsered with at least one character selected 
+// password that matches criteria created 
+// password 
+// // WHEN the password is generated
+// // THEN the password is either displayed in an alert or written to the page
+
+
+
+
