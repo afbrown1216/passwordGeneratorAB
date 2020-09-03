f1bdd46956240aa71427ba254669a3685aeb2e71
Phantom <fay.ashbro@gmail.com>
2020-09-02 23:16:52 -0400

    prompts written, genpass object created

--- script.js
+++ script.js
@@ -17,22 +17,43 @@ function generatePassword() {
   
   // length between 8-128
   passwordLength = prompt("Enter a length between 8 and 128 characters:")
-  if(passwordLength <8 || passwordLength > 128){
-    prompt("Enter")
+  if(passwordLength >=8 || passwordLength <= 128){
+    prompt("You must enter a number between 8 and 128 characters:")
   } // else move on 
 
   // character types 
 // lowercase
-lowerPassword = prompt("")
+  lowerPassword = prompt("Do you want to include a lowercase character?")
+  //conditional
 //uppercase
-// // upperPassword = prompt 
+  upperPassword = prompt("Do you want to include an uppercase character?")
+  //conditional
 // numeric
-// // num = prompt 
+num = prompt("Do you want to include an numeric character?")
+//conditional
 // special characters 
-// // specChar = 
+specChar = prompt("Do you want to include a special character?")
+//conditonal 
+
+//validate the inputs at least one character type should be slected 
+
+//generate password 
+//return password 
 
 
 }
+
+
+
+genpassword = {
+  lowercase : ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"],
+  uppercase: ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"],
+  numeric: [123456789],
+  specialCharacters: 
+  [" !#$%&'()*+,-./:;<=>?@[\]^_`{|}~"],
+
+}
+
 
 
 //   // prompts for password 
