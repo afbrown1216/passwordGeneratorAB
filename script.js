252c433d07930559f3b70630945f690df65ee9d8
Phantom <fay.ashbro@gmail.com>
2020-09-03 20:33:39 -0400

    for created to generate password.undefined returned

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var passArray = [];
+var availableCharacters = "";
+var newPassword;
 
 // Character generation variables 
-var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var numeric = [1,2,3,4,5,6,7,8,9];
-var specialCharacters = [" !#$%&'()*+,-./:;<=>?@[\]^_`{|}~ "];
+var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numeric = "123456789";
+var specialCharacters = " !#$%&'()*+,-./:;<=>?@[\]^_`{|}~ ";
 
 // Write password to the #password input
 function writePassword() {
@@ -26,7 +27,7 @@ function generatePassword() {
   // length between 8-128
   passwordLength = prompt("Pick a character count between 8 and 128 for your password:")
   if (passwordLength < 8 || passwordLength > 128) {
-    prompt("You must enter a number between 8 and 128 characters:")
+    prompt("Please enter a number between 8 and 128 characters:")
   } 
   console.log("Password Character length: ", passwordLength)
  //conditional if true generate a password with this many characters 
@@ -34,46 +35,46 @@ function generatePassword() {
   // lowercase
   var lCCharacter = confirm("Do you want to include a lowercase character?");
   if(lCCharacter){
-    passArray.push(lowerCase);
+    availableCharacters = availableCharacters + lowerCase;
   }
   console.log("Lowercase Characters included: ", lCCharacter);
-  console.log("Password Characters available: ",passArray);
+  // console.log("Password Characters available: ",passArray);
   //conditional if true generate a password with lowercase character
   //else do not generate a password with a lowercase character 
   //create a function for a password with a lowercase character 
   //uppercase
   var uCCharacter = confirm("Do you want to include an uppercase character?");
   if(uCCharacter){
-    passArray.push(upperCase);
+    availableCharacters = availableCharacters + upperCase;
   }
 
   console.log("Uppercase Characters included: ", uCCharacter);
-  console.log("Password Characters available: ",passArray);
+  // console.log("Password Characters available: ",passArray);
   //conditional if true generate a password with uppercase character 
   //elsef do not generate a password with a  lowercase character
   //create a function for a password with a uppercase character 
   // numeric
   var num = confirm("Do you want to include an numeric character?");
   if (num){
-    passArray.push(numeric);
+    availableCharacters = availableCharacters + numeric;
   }
   console.log("Numeric Characters included: ", num);
-  console.log("Password Characters available: ", passArray);
+  // console.log("Password Characters available: ", passArray);
   //conditional if true generate a password with a numeric character 
   //else do not genrate a password with a numeric character 
   //create a function for a password with a
   // special characters 
   var specChar = confirm("Do you want to include a special characters?");
   if (specChar) {
-    passArray.push(specialCharacters);
+    availableCharacters = availableCharacters + specChar;
   }
   console.log("Special Characters included: ", specChar);
-  console.log("Password Characters available: ", passArray);
+  // console.log("Password Characters available: ", passArray);
 
 
   //validate the inputs at least one character type should be selected might put outside to call over again
   var confirmPassword = confirm(
-    "Created a password with the following characters:  Lowercase" + lCCharacter
+    "Created a password with the following characters:  Lowercase " + lCCharacter
      + " Uppercase " + uCCharacter
      + " Numeric " + num 
      + "Special Characters " + specChar)
@@ -82,10 +83,14 @@ function generatePassword() {
     }
 
   //number generation 
-    for (i = 1; i > passwordLength; i++ ){
+    for (i = 0; i < passwordLength; i++ ){
 
-      var randomIndex = Math.floor(Math.random() * passArray.length) 
+      var randomNumber = Math.floor(Math.random() * availableCharacters.length);
+      
+      var randomCharacter = availableCharacters[randomNumber];
     
+     newPassword = newPassword + randomCharacter;
+     
     
     } 
  //create a function for a password with a a special character
@@ -94,7 +99,7 @@ function generatePassword() {
 
   //generate password 
   //return password 
-
+    return newPassword 
 
 }
 
